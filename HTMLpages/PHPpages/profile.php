78789bafe140a933bd31a8d270f478ff9a003576
callmeAyanda <a22.makhubu@gmail.com>
2024-09-01 15:22:42 +0200

    created basic structure of main user profile

--- HTMLpages/PHPpages/profile.php
+++ HTMLpages/PHPpages/profile.php
@@ -17,29 +17,3 @@ $stmt_posts->bind_param("i", $user_id);
 $stmt_posts->execute();
 $posts_result = $stmt_posts->get_result();
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Profile</title>
-    <link rel="stylesheet" href="CSSpages/profile.css">
-</head>
-<body>
-    <h2>My Profile</h2>
-    <div class="user-info">
-        <p>Name: <?php echo $user['firstname'] . " " . $user['lastname']; ?></p>
-        <p>Email: <?php echo $user['email']; ?></p>
-    </div>
-    <div class="user-posts">
-        <h3>My Posts</h3>
-        <?php while ($post = $posts_result->fetch_assoc()): ?>
-            <div class="post">
-                <img src="<?php echo $post['image_path']; ?>" alt="Post Image">
-                <p><?php echo $post['caption']; ?></p>
-            </div>
-        <?php endwhile; ?>
-    </div>
-</body>
-</html>
